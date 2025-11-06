b84d9329f07ee94fc00bec1ed83268b8cb14fdd0
Uttam <kumaruttam519@gmail.com>
2025-11-07 01:51:00 +0530

    chore: set up the server.js file

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const app = require("./index");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
